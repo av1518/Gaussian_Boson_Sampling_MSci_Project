2f677b70a86c9ed185c552bcfce33e02ee85ba44
Gefaell <jose.gefa11@gmail.com>
2023-02-06 15:54:01 +0000

    updated function names

--- plot_greedy_success.py
+++ plot_greedy_success.py
@@ -1,48 +1,36 @@
 #%%
-import strawberryfields as sf
-from strawberryfields import ops
 import numpy as np
-from typing import Tuple, List
-import itertools as iter
-from utils import bitstring_to_int, convert_to_clicks
-from itertools import combinations
+from utils import total_variation_distance
 from gbs_simulation import GBS_simulation
 from scipy.stats import unitary_group
 from greedy import Greedy
-from GBS_marginals import Marginal
+from gbs_probabilities import TheoreticalProbabilities
 from tqdm import tqdm
 import matplotlib.pyplot as plt 
 
 # %%
 
 n_modes = 4
-
 r_k = [0.5] * n_modes
-
 U = unitary_group.rvs(n_modes) 
 
 #%%
 gbs = GBS_simulation()
 greedy = Greedy()
+probs = TheoreticalProbabilities()
 
-
-ideal_marg_tor = Marginal().get_ideal_marginals_from_torontonian(n_modes,r_k,U,2)
-
-
+ideal_marg_tor = probs.get_all_ideal_marginals_from_torontonian(n_modes,r_k,U,2)
 greedy_matrix = greedy.get_S_matrix(n_modes, 500, 2, ideal_marg_tor)
 greedy_dist = greedy.get_distribution_from_outcomes(greedy_matrix)
 
 cutoff = 5
-loss = np.linspace(0, np.pi/2, 15 )
+loss = np.linspace(0, 1, 15 )
 
 distances = []
 for i in tqdm(loss):  
-    ideal_dist = gbs.get_noisy_marginal_from_simul(n_modes, cutoff, r_k, U,list(range(n_modes)), i)
-    distance = 0.5 * np.sum(np.abs(ideal_dist - greedy_dist))
+    ideal_dist = gbs.get_noisy_marginal_from_simulation(n_modes, cutoff, r_k, U,list(range(n_modes)), i)
+    distance = total_variation_distance(ideal_dist, greedy_dist)
     distances.append(distance)
-
-
-
 
 plt.plot(loss, distances, 'o-', label = f'modes = {n_modes}, cutoff = {cutoff} ')
 plt.xlabel(r'Noise ($\phi$) ')
