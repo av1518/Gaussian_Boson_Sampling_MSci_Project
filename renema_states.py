0681e042fbeffa0cdb44e460aac30296e912a88e
Gefaell <jose.gefa11@gmail.com>
2023-02-14 00:33:01 +0000

    renema_states working version

--- renema_states.py
+++ renema_states.py
@@ -13,33 +13,46 @@ from utils import bitstring_to_int, convert_to_clicks
 import itertools as iter
 from itertools import combinations
 
-n_modes = 4
-cutoff = 6
+n_modes = 6
+cutoff = 4
 k_order = 2
-L = 500
+L = 1000
 unitary = unitary_group.rvs(n_modes)
 
 def ideal_renema_circuit_10(n_modes, unitary):
+    num_non_vacuum_modes = int(np.ceil(0.5*n_modes))
+    index = np.zeros((n_modes,), dtype=np.int16)
+    index[:num_non_vacuum_modes] = 1
+    ket = np.zeros([cutoff]*n_modes, dtype=np.complex128)
+    ket[tuple(index)] = 1.0 + 1j*0.0
     prog = sf.Program(n_modes)
     with prog.context as q:
-        for i in range(int(np.ceil(0.5*n_modes))):
-            ops.Fock(1) | q[i]
+        ops.Ket(ket) | q
         ops.Interferometer(unitary) | q
     return prog
 
 def ideal_renema_circuit_01(n_modes, unitary):
+    num_non_vacuum_modes = int(np.ceil(0.5*n_modes))
+    index = np.zeros((n_modes,), dtype=np.int16)
+    index[num_non_vacuum_modes:] = 1
+    ket = np.zeros([cutoff]*n_modes, dtype=np.complex128)
+    ket[tuple(index)] = 1.0 + 1j*0.0
     prog = sf.Program(n_modes)
     with prog.context as q:
-        for i in range(int(np.ceil(0.5*n_modes)), n_modes):
-            ops.Fock(1) | q[i]
+        ops.Ket(ket) | q
         ops.Interferometer(unitary) | q
     return prog
 
 def noisy_renema_circuit_10(n_modes, unitary, loss):
+    loss = loss*np.pi/2
+    num_non_vacuum_modes = int(np.ceil(0.5*n_modes))
+    index = np.zeros((2*n_modes,), dtype=np.int16)
+    index[:num_non_vacuum_modes] = 1
+    ket = np.zeros([cutoff]*2*n_modes, dtype=np.complex128)
+    ket[tuple(index)] = 1.0 + 1j*0.0
     prog = sf.Program(2*n_modes)
     with prog.context as q:
-        for i in range(int(np.ceil(0.5*n_modes))):
-            ops.Fock(1) | q[i]
+        ops.Ket(ket) | q
         for cmd in ops.Interferometer(unitary).decompose(tuple([qubit for qubit in q[:n_modes]])):
             cmd.op | cmd.reg
         for i, qubit in enumerate(q[:n_modes]):
@@ -47,10 +60,15 @@ def noisy_renema_circuit_10(n_modes, unitary, loss):
     return prog
 
 def noisy_renema_circuit_01(n_modes, unitary, loss):
+    loss = loss*np.pi/2
+    num_non_vacuum_modes = int(np.ceil(0.5*n_modes))
+    index = np.zeros((2*n_modes,), dtype=np.int16)
+    index[num_non_vacuum_modes:] = 1
+    ket = np.zeros([cutoff]*2*n_modes, dtype=np.complex128)
+    ket[tuple(index)] = 1.0 + 1j*0.0
     prog = sf.Program(2*n_modes)
     with prog.context as q:
-        for i in range(int(np.ceil(0.5*n_modes)), n_modes):
-            ops.Fock(1) | q[i]
+        ops.Ket(ket) | q
         for cmd in ops.Interferometer(unitary).decompose(tuple([qubit for qubit in q[:n_modes]])):
             cmd.op | cmd.reg
         for i, qubit in enumerate(q[:n_modes]):
@@ -65,7 +83,6 @@ def get_state_vector_from_program(
     and obtains the threshold marginal distribution of the specified target modes."""
     eng = sf.Engine("fock", backend_options={"cutoff_dim": fock_cutoff})
     result = eng.run(program)
-    print(result.state)
     fock_ket = result.state.ket()
     return fock_ket
 
@@ -109,8 +126,8 @@ def get_renema_output_statevec(n_modes, unitary, cutoff):
     return superposition_ket
 
 def get_noisy_renema_output_statevec(n_modes, unitary, cutoff, loss):
-    ket1 = get_state_vector_from_program(noisy_renema_circuit_01(n_modes, unitary), cutoff, loss)
-    ket2 = get_state_vector_from_program(noisy_renema_circuit_10(n_modes, unitary), cutoff, loss)
+    ket1 = get_state_vector_from_program(noisy_renema_circuit_01(n_modes, unitary, loss), cutoff)
+    ket2 = get_state_vector_from_program(noisy_renema_circuit_10(n_modes, unitary, loss), cutoff)
     superposition_ket = (1/np.sqrt(2))*(ket1 + ket2)
     return superposition_ket
 
@@ -121,20 +138,36 @@ greedy = Greedy()
 ideal_margs = get_all_ideal_marginals_from_statevec(n_modes, get_renema_output_statevec(n_modes, unitary, cutoff), k_order)
 greedy_matrix = greedy.get_S_matrix(n_modes, L, k_order, ideal_margs)
 greedy_distr = greedy.get_distribution_from_outcomes(greedy_matrix)
+print(greedy_distr)
+ket = get_renema_output_statevec(n_modes, unitary, cutoff)
+ideal_distr = np.array(get_threshold_marginal_from_statevec(ket, list(range(n_modes))))
+print(ideal_distr)
+distance = total_variation_distance(ideal_distr, greedy_distr)
+print(distance)
 
-#%%
-loss = np.linspace(0, 1, 15 )
-
-distances = []
-for i in tqdm(loss):
-    ket = get_noisy_renema_output_statevec(n_modes, unitary, cutoff, i)
-    ideal_distr = get_threshold_marginal_from_statevec(ket, list(range(n_modes)))
-    distance = total_variation_distance(ideal_distr, greedy_distr)
-    distances.append(distance)
-
-plt.plot(loss, distances, 'o-', label = f'modes = {n_modes}, cutoff = {cutoff} ')
-plt.xlabel(r'Noise ($\phi$) ')
-plt.ylabel(r'$\mathcal{D}$(Greedy,Ground)')
+x = list(range(2**n_modes))
+plt.plot(x, greedy_distr, label='Greedy')
+plt.plot(x, ideal_distr, label='Ideal')
+plt.xlabel('Bitstring')
+plt.ylabel('Probability')
 plt.legend()
 plt.show()
+
+
+#%%
+# loss = np.linspace(0, 1, 13)
+
+# distances = []
+# for i in tqdm(loss):
+#     ket = get_noisy_renema_output_statevec(n_modes, unitary, cutoff, i)
+#     ideal_distr = np.array(get_threshold_marginal_from_statevec(ket, list(range(n_modes))))
+#     print(ideal_distr)
+#     distance = total_variation_distance(ideal_distr, greedy_distr)
+#     distances.append(distance)
+
+# plt.plot(loss, distances, 'o-', label = f'Modes = {n_modes}, Cutoff = {cutoff} ')
+# plt.xlabel('Loss')
+# plt.ylabel(r'$\mathcal{D}$(Greedy,Ground)')
+# plt.legend()
+# plt.show()
 
