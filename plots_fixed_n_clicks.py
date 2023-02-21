e359e084ee60427aeb22e34a0f1e58be254cfc91
Gefaell <jose.gefa11@gmail.com>
2023-02-21 16:52:52 +0000

    minor updates

--- plots_fixed_n_clicks.py
+++ plots_fixed_n_clicks.py
@@ -10,14 +10,15 @@ from tqdm import tqdm
 import matplotlib.pyplot as plt
 from utils import bitstring_to_int
 
-n_modes = 3
-r_k = [0.5] * n_modes
-U = unitary_group.rvs(n_modes) 
-cutoff = 7
-loss = np.linspace(0, 1, 10 )
-n_fixed = int(n_modes/2)
+# n_modes = 4
+# r_k = [0.4] * n_modes
+# U = unitary_group.rvs(n_modes, random_state=1) 
+# cutoff = 7
+# loss = np.linspace(0, 1, 10 )
+# n_fixed = int(n_modes/2)
+# L = 2000
 
-#%%
+# #%%
 gbs = GBS_simulation()
 greedy = Greedy()
 probs = TheoreticalProbabilities()
@@ -43,30 +44,30 @@ def get_distribution_from_outcomes(samples: np.ndarray) -> np.ndarray:
     distribution = np.array(counts) / np.sum(counts)
     return list(count_dict.keys()), distribution
 
-ideal_marg_tor = probs.get_all_ideal_marginals_from_torontonian(n_modes,r_k,U,2)
-greedy_matrix = greedy.get_S_matrix(n_modes, 500, 2, ideal_marg_tor)
-fixed_n_clicks_submatrix = get_submatrix_with_fixed_n_clicks(greedy_matrix, n_fixed)
-subset, greedy_distr = get_distribution_from_outcomes(fixed_n_clicks_submatrix)
-print('Bitstring subset (in decimal):', subset)
+#ideal_marg_tor = probs.get_all_ideal_marginals_from_torontonian(n_modes,r_k,U,2)
+# greedy_matrix = greedy.get_S_matrix(n_modes, L, 2, ideal_marg_tor)
+# fixed_n_clicks_submatrix = get_submatrix_with_fixed_n_clicks(greedy_matrix, n_fixed)
+# subset, greedy_distr = get_distribution_from_outcomes(fixed_n_clicks_submatrix)
+# print('Bitstring subset (in decimal):', subset)
+
+# #%%
+# distances = []
+# for i in tqdm(loss):  
+#     ground_distr = gbs.get_lossy_marginal_from_gaussian_simulation(n_modes, cutoff, r_k, U,list(range(n_modes)), i)
+#     conditional_probs = [x for i, x in enumerate(ground_distr) if i in subset]
+#     ground_distr = np.array(conditional_probs)/np.sum(conditional_probs)
+#     distance = total_variation_distance(ground_distr, greedy_distr)
+#     distances.append(distance)
+
+# plt.figure()
+# plt.plot(loss, distances, 'o-', label = f'Modes = {n_modes}, Cutoff = {cutoff} ')
+# plt.xlabel('Loss')
+# plt.ylabel(r'$\mathcal{D}$(Greedy,Ground)')
+# plt.legend()
+# plt.show()
 
 #%%
-distances = []
-for i in tqdm(loss):  
-    ground_distr = gbs.get_lossy_marginal_from_gaussian_simulation(n_modes, cutoff, r_k, U,list(range(n_modes)), i)
-    conditional_probs = [x for i, x in enumerate(ground_distr) if i in subset]
-    ground_distr = np.array(conditional_probs)/np.sum(conditional_probs)
-    distance = total_variation_distance(ground_distr, greedy_distr)
-    distances.append(distance)
-
-plt.figure()
-plt.plot(loss, distances, 'o-', label = f'Modes = {n_modes}, Cutoff = {cutoff} ')
-plt.xlabel('Loss')
-plt.ylabel(r'$\mathcal{D}$(Greedy,Ground)')
-plt.legend()
-plt.show()
-
-#%%
-n_modes = 5
+n_modes = 4
 r_k = [0.5] * n_modes
 U = unitary_group.rvs(n_modes) 
 cutoff = 7
