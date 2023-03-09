984ab0443374c0e2cc8967d3ccdd0a04201aa2c4
av1518 <65544264+av1518@users.noreply.github.com>
2023-03-09 09:36:32 +0000

    some changes

--- greedy_optimisation.py
+++ greedy_optimisation.py
@@ -14,28 +14,38 @@ n_modes = 3
 r_k = [0.4] * n_modes
 U = unitary_group.rvs(n_modes) 
 cutoff = 7
-loss = np.linspace(0, 1, 10 )
 
 #%%
 gbs = GBS_simulation()
 greedy = Greedy()
 probs = TheoreticalProbabilities()
+n_samples = np.linspace(200,1000,10)
 
 ideal_marg_tor = probs.get_all_ideal_marginals_from_torontonian(n_modes,r_k,U,2)
-greedy_matrix = greedy.get_S_matrix(n_modes, 500, 2, ideal_marg_tor)
-greedy_dist = greedy.get_distribution_from_outcomes(greedy_matrix)
+
+
+# distances = []
+# for i in tqdm(loss):  
+#     ideal_dist = gbs.get_noisy_marginal_from_simulation(n_modes, cutoff, r_k, U,list(range(n_modes)), i)
+#     distance = total_variation_distance(ideal_dist, greedy_dist)
+#     distances.append(distance)
 
 distances = []
-for i in tqdm(loss):  
-    ideal_dist = gbs.get_noisy_marginal_from_simulation(n_modes, cutoff, r_k, U,list(range(n_modes)), i)
+for i in tqdm(n_samples):  
+    print(i)
+    greedy_matrix = greedy.get_S_matrix(n_modes, int(i), 2, ideal_marg_tor)
+    greedy_dist = greedy.get_distribution_from_outcomes(greedy_matrix)
+    ideal_dist = gbs.get_noisy_marginal_from_simulation(n_modes, cutoff, r_k, U,list(range(n_modes)), 0)
     distance = total_variation_distance(ideal_dist, greedy_dist)
     distances.append(distance)
 
+#%%
+
 plt.figure()
-plt.plot(loss, distances, 'o-', label = f'Modes = {n_modes}, Cutoff = {cutoff} ')
-plt.xlabel('Loss')
+plt.plot(n_samples, distances, 'o-', label = f'Modes = {n_modes}, Cutoff = {cutoff} ')
+plt.xlabel('n_samples')
 plt.ylabel(r'$\mathcal{D}$(Greedy,Ground)')
 plt.legend()
 plt.show()
 
-# np.save('distances_greedy,ground_n=3_cut=8', distances)
+np.save('distances_greedy,ground_n=3_cut=8', distances)
--- plot_greedy_success.py
+++ plot_greedy_success.py
@@ -7,22 +7,25 @@ from greedy import Greedy
 from gbs_probabilities import TheoreticalProbabilities
 from tqdm import tqdm
 import matplotlib.pyplot as plt 
+from scipy.optimize import curve_fit
+
 
 # %%
 
-n_modes = 3
+n_modes = 5
 r_k = [0.4] * n_modes
 U = unitary_group.rvs(n_modes) 
 cutoff = 7
-loss = np.linspace(0, 1, 10 )
-
+n_points = 30
+loss = np.linspace(0, 1, n_points )
+L = 1000
 #%%
 gbs = GBS_simulation()
 greedy = Greedy()
 probs = TheoreticalProbabilities()
 
 ideal_marg_tor = probs.get_all_ideal_marginals_from_torontonian(n_modes,r_k,U,2)
-greedy_matrix = greedy.get_S_matrix(n_modes, 500, 2, ideal_marg_tor)
+greedy_matrix = greedy.get_S_matrix(n_modes, L, 2, ideal_marg_tor)
 greedy_dist = greedy.get_distribution_from_outcomes(greedy_matrix)
 
 distances = []
@@ -31,14 +34,17 @@ for i in tqdm(loss):
     distance = total_variation_distance(ideal_dist, greedy_dist)
     distances.append(distance)
 
+#%%
+# distances = np.load('distances_greedy,ground_n=5_cut=7_samples=1000.npy',allow_pickle=True)
+
 plt.figure()
-plt.plot(loss, distances, 'o-', label = f'Modes = {n_modes}, Cutoff = {cutoff} ')
+plt.plot(loss, distances, 'o-', label = f'Modes = {n_modes}, Cutoff = {cutoff}, Samples = {L} ')
 plt.xlabel('Loss')
 plt.ylabel(r'$\mathcal{D}$(Greedy,Ground)')
 plt.legend()
 plt.show()
 
-# np.save('distances_greedy,ground_n=3_cut=8', distances)
+np.save(f'distances_greedy,ground_n={n_modes}_cut={cutoff}_samples={L}_N={n_points}', distances)
 
 # %% Scale squeezing parameters to account for photon loss
 
@@ -101,18 +107,21 @@ gbs = GBS_simulation()
 greedy = Greedy()
 probs = TheoreticalProbabilities()
 n_modes = 4
-U = unitary_group.rvs(n_modes) 
+U = unitary_group.rvs(n_modes,random_state=1) 
 r_k = [0.4] * n_modes
 
 
 ideal_marg_tor = probs.get_all_ideal_marginals_from_torontonian(n_modes,r_k,U,2)
-greedy_matrix = greedy.get_S_matrix(n_modes, 500, 2, ideal_marg_tor)
+greedy_matrix = greedy.get_S_matrix(n_modes, 1000, 2, ideal_marg_tor)
 greedy_distr = greedy.get_distribution_from_outcomes(greedy_matrix)
 
+#%%
 cutoff = 6
-stddev = np.linspace(0, 1, 20)
-repetitions = 50
-
+range_n = 0.3
+n_points = 30
+stddev = np.linspace(0, range_n, n_points)
+repetitions = 100
+#%%
 distances = []
 for i in tqdm(stddev):  
     initial_ideal_distr = np.array(gbs.get_noisy_marginal_gate_error(cutoff, r_k, U, list(range(n_modes)), i))
@@ -127,7 +136,16 @@ for i in tqdm(stddev):
     distances.append(distance)
 
 #%%
-plt.plot(stddev, distances, 'o-', label = f'modes = {n_modes}, cutoff = {cutoff} ')
+# distances = np.load('distances_greedy,ground_n=4_cut=6,repetitions=100,range =0.1,_gate_error.npy',allow_pickle=True)
+#%%
+def f(x,a,b):
+    return a*x**2 + b 
+
+popt,pcov = curve_fit(f,stddev,distances)
+
+
+plt.plot(stddev, distances, 'o-', label = f'modes = {n_modes}, cutoff = {cutoff}, repetitions = {repetitions} ')
+plt.plot(stddev, f(stddev,popt[0],popt[1]))
 plt.xlabel(r'Standard Deviation ')
 plt.ylabel(r'$\mathcal{D}$($Greedy$,$\overline{Ground}$)')
 plt.legend()
@@ -135,7 +153,8 @@ plt.legend()
 
 
 #%%
-np.save(f'distances_greedy,ground_n={n_modes}_cut={cutoff}_gate_error', distances)
+
+np.save(f'distances_greedy,ground_n={n_modes}_cut={cutoff},repetitions={repetitions},range ={range_n},_gate_error,numberofpoints= {n_points}', distances)
 plt.savefig('gate error model vs greedy')
 #%%
 distances = []
